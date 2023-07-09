5082d9168a6495d82fa7df8d600eeb2e046b9542
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-09 17:42:00 -0400

    added comments to explain the code logic

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //Color Switcher Project
 
 function colorSelector(value){
-	document.body.style.backgroundColor = value
+//function uses the color value from the selector options as a parameter
+
+	document.body.style.backgroundColor = value;
+/*
+	the background color of the doucment (webpage) 
+	is equal to the value of the selector option
+*/
 }
